36131af8193e4fb2796195f8397bc4a063740a32
Marcin <marcinkacki94@gmail.com>
2020-03-20 09:42:10 +0100

    init

---
+++ js/tabelka-zadanie.js
@@ -0,0 +1,9 @@
+let users = ["Ola", "Ania", "Tomek", "Bartek"]
+
+users.forEach(function(elem) {
+    let listItems = document.createElement("li");
+    listItems.innerText = elem;
+    listItems.classList.add("user");
+    document.getElementsByClassName("users-list")[0].appendChild(listItems);
+
+})
